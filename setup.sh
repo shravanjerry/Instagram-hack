72710f8fba28827413c62bce7a0079b7bafa5ccc
shravanjerry <66510252+shravanjerry@users.noreply.github.com>
2020-07-04 11:50:41 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,42 +18,7 @@ systemctl start apache2
 apt install xterm -y
 apt install gnome-terminal -y
 unzip ngrok-stable-linux-amd64.zip
-clear
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-unzip ngrok-stable-linux-amd64.zip
-cp -R ngrok sites/github/
-cp -R ngrok sites/Hotstar-otp-bypass/
-cp -R ngrok sites/instagram/
-cp -R ngrok sites/Linkedin/
-cp -R ngrok sites/Netflix/
-cp -R ngrok sites/Paytm-Phishing/paytm/
-cp -R ngrok sites/Paytm-Phishing/signup/
-cp -R ngrok sites/spotify/
-cp -R ngrok sites/whatsapp-phishing/
-cp -R ngrok sites/facebook/
-cp -R ngrok sites/google-otp/
-cp -R ngrok sites/instafollow/
-cp -R ngrok sites/ipfinder/
-cp -R ngrok sites/ola-otpbypass/
-cp -R ngrok sites/UberEats-Phishing/
-cp -R ngrok sites/Zomato-Phishing/
-cp -R ngrok sites/amazonsign/
-cp -R ngrok sites/tiktok/
-cp -R ngrok sites/phonepay/
-cp -R ngrok sites/paypal/
-cp -R ngrok sites/telegram/
-cp -R ngrok sites/twitter/
-cp -R ngrok sites/flipcart/
-cp -R ngrok sites/wordpress/
-cp -R ngrok sites/snapchat/
-cp -R ngrok sites/protonmail/
-cp -R ngrok sites/stackoverflow/
-cp -R ngrok sites/ebay/
-cp -R ngrok sites/twitch/
-cp -R ngrok sites/ajio/
-cp -R ngrok sites/cryptocurrency/
-cp -R ngrok sites/mobikwik/
-cp -R ngrok sites/pinterest/
+clea
 chmod 7777 AdvPhishing.sh
 clear
 echo ""
