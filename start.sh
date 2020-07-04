3cbf2c531ba3041f3a72b3b9e378925c33955c1b
shravanjerry <66510252+shravanjerry@users.noreply.github.com>
2020-07-04 16:12:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,33 @@
 echo ""
 clear
 echo ""
+limit=100
+sleep 1
+echo " WELCOME TO YOU"
+sleep 1
+echo -n "what is your Name?:"
+read Name;
+sleep 2
+echo "Hello Mr/ Mrs [$Name]"
+sleep 1
+clear
+echo -n "Did you subscribed my youtube channel ? y/n :"
+read pilih
+if [ $pilih = "y" ];
+then
+sleep 2
+echo "Thanks for subscribing my channel"
+sleep 2
+clear
+elif [ $pilih = "n" ];
+then
+sleep 2
+echo "Please SUBSCRIBE CHANNEL My channel"
+termux-open-url "https://www.youtube.com/channel/UCw5u7r2zZOJgPSe-3qyuOag"
+sleep 4
+sleep 2
+fi
+clear
 echo ""
 echo ""
 echo ""
@@ -31,4 +58,5 @@ echo ""
 ./ngrok authtoken 1dZVrxwoGwWjEMP4a9IoYdv0Tig_2jVL3oEUPfrrBcZjcXTYH
 sleep 2
 clear
+./Advinstagramhack.sh
 
